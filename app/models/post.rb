11a1df832e4853931e3b12da14a029f51a885a8c
John Anthony <askinganthony@gmail.com>
2016-02-01 18:02:10 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,15 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_tags
   has_many :tags, :through => :post_tags
+  accepts_nested_attributes_for :tags, reject_if: proc { |attributes| attributes['name'].blank? }
 
   validates_presence_of :name, :content
+
+  def tag=(tag_params)
+  	require 'pry'
+  	binding.pry
+  	new_tag=Tag.find_or_create_by(tag_params) unless tag_params[:name]=''
+  	self.tags << new_tag
+  end
+
 end
