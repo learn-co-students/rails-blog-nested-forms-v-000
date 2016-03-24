a0417b56b9652d5ac7ad12095d2248b44d1aa049
Dakota Martinez <dakotaleemusic@gmail.com>
2016-03-24 19:53:00 +0000

    adds new tag feature to posts form

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,11 @@ class Post < ActiveRecord::Base
   has_many :post_tags
   has_many :tags, :through => :post_tags
   accepts_nested_attributes_for :tags, reject_if: proc { |attributes| attributes[:name].blank? }
+  
+  def tags_attributes=(tags_attributes)
+    tag = Tag.find_or_create_by(tags_attributes)
+    self.tags << tag
+  end
 
   validates_presence_of :name, :content
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ RailsBlog::Application.routes.draw do
   resources :users
   resources :tags
   resources :posts
+  
+  root 'posts#index'
 
 end
