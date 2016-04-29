ca7e136c1c207a379093f963f00d0355be23f1ab
glee38 <glee3388@gmail.com>
2016-04-28 22:43:39 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,16 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_tags
   has_many :tags, :through => :post_tags
+  accepts_nested_attributes_for :tags, reject_if: :all_blank
 
   validates_presence_of :name, :content
+
+  # def tags_attributes=(tags_attributes)
+  #   tags_attributes.each do |i, tag_attribute|
+  #     if tag_attribute[:name].present?
+  #       tag = Tag.find_or_create_by(name: tag_attribute[:name])
+  #       self.tags << tag
+  #     end
+  #   end
+  # end
 end
