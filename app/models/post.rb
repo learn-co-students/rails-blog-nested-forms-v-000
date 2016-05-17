d0d756a519039a94a7a110f27caf2ca4788586cd
McN33ly <rmcneely23@gmail.com>
2016-05-17 11:39:55 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,5 +5,14 @@ class Post < ActiveRecord::Base
   has_many :tags, :through => :post_tags
 
   validates_presence_of :name, :content
-    accepts_nested_attributes_for :tags, reject_if: :attributes['name'].blank?
+  accepts_nested_attributes_for :tags, reject_if: :all_blank
+
+  def tag_attributes=(tag_attributes)
+    tag_attributes.values.each do |attribute|
+      if !attribute.blank?
+      tag = Tag.create(name: attribute)
+      self.tags << tag
+      end
+    end
+  end
 end
