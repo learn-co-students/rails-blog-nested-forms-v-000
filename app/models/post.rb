2a656f1ade3477852e126cf128b466f517fb3878
jenjiyi <jenny.yamada@gmail.com>
2016-05-19 12:13:24 -0700

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,22 @@ class Post < ActiveRecord::Base
   has_many :post_tags
   has_many :tags, :through => :post_tags
 
+
   validates_presence_of :name, :content
+
+accepts_nested_attributes_for :tags, reject_if: lambda {|attributes| attributes['name'].blank?}
+  
+  # def tags_attributes=(tags_hashes)
+  #   tags_hashes.each do |i, tag_attributes|
+  #     if tag_attributes[:name].present?
+  #       tag = Tag.find_or_create_by(name: tag_attributes[:name])
+  #       if !self.tags.include?(tag)
+  #         self.post_tags.build(:tag => tag)
+  #       end
+
+  #     end
+
+  #   end
+  # end
+
 end
