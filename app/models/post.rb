8b683717758356e78e48e4557083865acdd5084a
mcclains2003 <mcclains2003@gmail.com>
2016-08-17 10:33:02 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,14 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_tags
   has_many :tags, :through => :post_tags
+  accepts_nested_attributes_for :tags, reject_if: lambda {|a| a['name'].blank?}
 
   validates_presence_of :name, :content
+
+  # def tags_attributes=(tags_attributes)
+  #   tags_attributes.each do |i, tag_attributes|
+  #     self.tags.build(tag_attributes)
+  #   end
+  # end
+
 end
