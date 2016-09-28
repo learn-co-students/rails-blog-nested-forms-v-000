f6e7dd28f2ecd7d6b4a0030c9f65a2137bd72ad2
ed-karabinus <ejkarabin@gmail.com>
2016-09-27 21:59:57 -0500

    Finished Rails Blog: Complex Nested Forms Lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,13 @@ class Post < ActiveRecord::Base
   has_many :tags, :through => :post_tags
 
   validates_presence_of :name, :content
+  
+  accepts_nested_attributes_for :tags
+
+  def tags_attributes=(tags_attributes)
+    tags_attributes.values.each do |tag_attributes|
+      tag = Tag.find_or_create_by(tag_attributes)
+      self.tags << tag
+    end unless tags_attributes.all? { |attr| attr.empty? }
+  end
 end
