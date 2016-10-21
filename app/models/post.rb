6f9f1e1224f7159634e564ce46e980612db0413c
Samuel Lair <lair001@gmail.com>
2016-10-21 00:31:13 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,10 @@ class Post < ActiveRecord::Base
   has_many :tags, :through => :post_tags
 
   validates_presence_of :name, :content
+  accepts_nested_attributes_for :tags, reject_if: proc { |attributes| attributes['name'].blank? }
+
+  def post_attributes=(post)
+    self.post = Post.find_or_create_by(name: post.name)
+    self.post.update(post)
+  end
 end
