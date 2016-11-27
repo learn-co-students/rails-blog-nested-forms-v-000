5459c5546b86d1f652e44b0e75629b74395c034b
Motti <=>
2016-11-26 22:16:23 -0500

    Done.

--- app/models/post_tag.rb
+++ app/models/post_tag.rb
@@ -2,3 +2,6 @@ class PostTag < ActiveRecord::Base
   belongs_to :post
   belongs_to :tag
 end
+
+
+# Whenever a new  tag  and post gets created it gets added to a join table based on that we can call has many 
