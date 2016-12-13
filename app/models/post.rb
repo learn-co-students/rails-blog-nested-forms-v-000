b533743f4320d044ff6cc55715f76be81d11b073
BrunoBoehm <bruno.boehm@gmail.com>
2016-12-13 11:06:06 +0100

    first version with <input type=text name=post[tag_attributes][] >

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,10 @@ class Post < ActiveRecord::Base
   has_many :tags, :through => :post_tags
 
   validates_presence_of :name, :content
+
+  def tag_attributes=(attributes)
+  	attributes.each do |attribute|
+  		self.tags << Tag.find_or_create_by(name: attribute) unless attribute.blank?
+  	end
+  end
 end
