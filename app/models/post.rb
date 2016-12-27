a1cb3057c082851f646c707918a5f768bf945106
github username <github email address>
2016-12-27 12:24:55 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,14 @@ class Post < ActiveRecord::Base
   has_many :tags, :through => :post_tags
 
   validates_presence_of :name, :content
+  #accepts_nested_attributes_for :tags, reject_if: proc { |attributes| attributes['name'].blank? }
+  def tags_attributes=(attributes)
+    attributes.each do |key,value|
+      if !value.blank?
+        tag = Tag.create(attributes)
+        self.tags << tag
+      end
+    end
+  end
+
 end
