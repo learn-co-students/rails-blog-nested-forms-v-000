e6a9e422641f7cef86bb9de04c9d29410a54c7d5
BrunoBoehm <bruno.boehm@gmail.com>
2017-01-24 18:20:48 +0100

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,9 +6,11 @@ class Post < ActiveRecord::Base
 
   validates_presence_of :name, :content
 
-  def tag_attributes=(attributes)
-  	attributes.each do |attribute|
-  		self.tags << Tag.find_or_create_by(name: attribute) unless attribute.blank?
-  	end
-  end
+  accepts_nested_attributes_for :tags, reject_if: proc { |attributes| attributes['name'].blank? }
+
+  # def tag_attributes=(attributes)
+  # 	attributes.each do |attribute|
+  # 		self.tags << Tag.find_or_create_by(name: attribute) unless attribute.blank?
+  # 	end
+  # end
 end
