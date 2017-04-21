688db401f67f40f9aca35a693c8da56870d65e3e
walwoodr <walwoodr@gmail.com>
2017-04-21 16:53:12 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,13 @@ class Post < ActiveRecord::Base
   has_many :tags, :through => :post_tags
 
   validates_presence_of :name, :content
+
+  def tags_attributes=(attributes_hash)
+    attributes_hash.each do |i, tag_attributes|
+      if tag_attributes.present? && tag_attributes['name'].present?
+        tag = Tag.find_or_create_by(tag_attributes)
+        self.post_tags.build(:tag_id => tag.id)
+      end
+    end
+  end
 end
