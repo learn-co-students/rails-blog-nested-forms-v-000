fbe1496d6baa825cf6b0bd4aa89a43c5f2e27304
github username <github email address>
2017-05-02 23:36:36 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,16 @@ class Post < ActiveRecord::Base
 
   validates_presence_of :name, :content
 
- accepts_nested_attributes_for :tags, reject_if: proc {|attribute| attribute[:name].blank? }
+ #accepts_nested_attributes_for :tags, reject_if: proc {|attribute| attribute[:name].blank? }
+
+ def tags_attributes=(tags_attributes) 		
+ 	tags_attributes.each do |i, tag_attribute|
+ 		if tag_attribute.present?
+	 		tag = Tag.find_or_create_by(tag_attribute)
+	 		self.post_tags.build(:tag => tag)
+	 	end
+ 	end
+ end
 
 
 
