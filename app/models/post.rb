3a305ef50214183303c19d37974727fb13e3dbff
anytimesoon <damien@delartigue.co.uk>
2017-05-06 16:09:28 +0100

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,14 @@ class Post < ActiveRecord::Base
   has_many :tags, :through => :post_tags
 
   validates_presence_of :name, :content
+  # accepts_nested_attributes_for :tags, reject_if: proc { |attrib| attrib['Name'].blank? }
+
+  def tags_attributes=(tags_hash)
+  	if !tags_hash.empty?
+  		tags_hash.each do |i, tag|
+  			self.tags.build(tag) unless tag == nil
+  		end
+  	end
+  end
+
 end
