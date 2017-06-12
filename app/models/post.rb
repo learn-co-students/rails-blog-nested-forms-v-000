bb3150862b274c9db254d448c484b94d0810901f
nadinesk <nadine.khattak@gmail.com>
2017-06-12 11:43:45 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,13 @@ class Post < ActiveRecord::Base
   has_many :tags, :through => :post_tags
 
   validates_presence_of :name, :content
+
+  accepts_nested_attributes_for :tags,  reject_if: proc { |attributes| attributes['name'].blank? }
+
+  #def tags_attributes=(name)
+   # tag_attributes.values.each do |name|
+    #  tag = Tag.find_or_create_by(name)
+     # self.tags << tag
+    #end
+  #end
 end
