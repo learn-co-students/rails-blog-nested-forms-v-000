5948cd9d07cce623a78337b3b79c71f8e03ce209
r0tiart <antley.chu@gmail.com>
2017-06-24 19:54:38 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,13 @@ class Post < ActiveRecord::Base
   has_many :tags, :through => :post_tags
 
   validates_presence_of :name, :content
+
+  def tags_attributes=(tag_attributes)
+  	tag_attributes.each do |i, attribute| 
+  		if !attribute.blank?
+  			tag = Tag.find_or_create_by(attribute)
+			self.tags << tag 		
+		end
+  	end
+  end
 end
