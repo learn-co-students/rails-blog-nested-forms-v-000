0bf047b4cfcdab136105289a1b41f8b628660521
jessica fletcher <cara.kane@gmail.com>
2017-08-01 18:16:22 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,14 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_tags
   has_many :tags, :through => :post_tags
+  accepts_nested_attributes_for :tags, reject_if: :all_blank
 
   validates_presence_of :name, :content
+
+  def tags_attributes=(tags_attributes)
+    tags_attributes.values.each do |tag_attributes|
+      tag = Tag.find_or_create_by(tag_attributes)
+      self.post_tags.build(:tag => tag)
+    end
+  end
 end
