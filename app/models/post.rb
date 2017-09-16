6386586e28d29a3f6f681a12aaeb7b63ac717bc0
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-09-16 16:18:02 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,14 +4,15 @@ class Post < ActiveRecord::Base
   has_many :post_tags
   has_many :tags, :through => :post_tags
 
-  #accepts_nested_attributes_for :tags
+  accepts_nested_attributes_for :tags, reject_if: lambda {|attributes| attributes['name'].blank?}
+
 
   validates_presence_of :name, :content
 
 
-  def tags_attributes=(tags_attributes)
-    tags_attributes.each do |key, tag_attributes|
-      self.tags.build(tag_attributes)
-    end
-  end
+  #def tags_attributes=(tags_attributes)
+  #  tags_attributes.each do |key, tag_attributes|
+  #    self.tags.build(tag_attributes)
+  #  end
+  #end
 end
