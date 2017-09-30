c9a1c09abab878d779608339e96909d292a78336
mikka <mikkanthrope@gmail.com>
2017-09-30 11:43:39 -0700

    Done.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -12,4 +12,18 @@ class TagsController < ApplicationController
     @tag = Tag.find(params[:id])
   end
 
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    @tag = Tag.new(tag_params)
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:name, tag_attributes: [:name])
+  end
+
 end
