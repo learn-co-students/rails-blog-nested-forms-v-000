2a5b25061882f0695aef2eb8fc2aebfcd286ac3b
Dalia <daliasawaya@tests-MacBook-Air-2.local>
2017-10-27 20:12:13 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,20 +4,17 @@ class Post < ActiveRecord::Base
   has_many :post_tags
   has_many :tags, :through => :post_tags
 
-  accepts_nested_attributes_for :tags
+  accepts_nested_attributes_for :tags, reject_if: proc { |attributes| attributes['name'].blank? }
 
   validates_presence_of :name, :content
 
-  def tags_attributes=(tag_attributes)
-    tag_attributes.each do |i,tag_attribute|
-    	# tag_attribute.each do |t|
-   		if tag_attribute && tag_attribute[:name]
-     	 	tag = Tag.find_or_create_by(tag_attribute) 
-     	 	self.tags << tag 
-     	 	puts tag_attribute
-     	 	puts tag
-    	end
-    end
-  end
+  # def tags_attributes=(tag_attributes)
+  #   tag_attributes.each do |i,tag_attribute| # i is here bc it was giving both the index and the value and WE DON'T KNOW WHY
+  #  		if tag_attribute && tag_attribute[:name]
+  #    	 	tag = Tag.find_or_create_by(tag_attribute) 
+  #    	 	self.tags << tag
+  #   	end
+  #   end
+  # end
 
 end
