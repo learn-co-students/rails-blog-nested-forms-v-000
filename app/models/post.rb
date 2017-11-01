39b5828110011feb2914baeb77ab859cb1639694
Kevin Yongchul Kim <github email address>
2017-11-01 15:09:29 -0700

    for f3mshep to look over

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,6 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_tags
   has_many :tags, :through => :post_tags
-  # accepts_nested_attributes_for :tags, reject_if: proc { |attributes| attributes['name'].blank? }
-  # won't work because we want to avoid duplicates in our tags
 
   validates_presence_of :name, :content
 
@@ -17,11 +15,4 @@ class Post < ActiveRecord::Base
       end
     end
   end
-
-
-end
-
-class Member < ActiveRecord::Base
-  has_many :posts
-  accepts_nested_attributes_for :posts, reject_if: proc { |attributes| attributes['title'].blank? }
 end
