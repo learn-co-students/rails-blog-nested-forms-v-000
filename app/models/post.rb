edc7756372935561bf7e52a75e56a665511cd9fd
Kevin Yongchul Kim <github email address>
2017-11-01 20:50:18 -0700

    done, form_for quirks not fully understood.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,16 +3,17 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_tags
   has_many :tags, :through => :post_tags
+  accepts_nested_attributes_for :tags, reject_if: :all_blank
 
   validates_presence_of :name, :content
 
-  def tags_attributes=(tag_attributes)
-    if tag_attributes[:name].present?
-      tag_attributes.values.each do |tag_attribute|
-        tag = Tag.find_or_create_by(name: tag_attribute)
-        self.post_tags.build(:tag => tag)
-        # self.tags << tag # inefficient alternative
-      end
-    end
-  end
+  # def tags_attributes=(tag_attributes)
+  #   if tag_attributes[:name].present?
+  #     tag_attributes.values.each do |tag_attribute|
+  #       tag = Tag.find_or_create_by(name: tag_attribute)
+  #       self.post_tags.build(:tag => tag)
+  #       # self.tags << tag # inefficient alternative
+  #     end
+  #   end
+  # end
 end
