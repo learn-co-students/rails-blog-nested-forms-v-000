db6282b25d61da52153301b15547deda23a21a61
mandlerm <amaya.melissa@gmail.com>
2017-12-06 16:29:22 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,17 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_tags
   has_many :tags, :through => :post_tags
-
   validates_presence_of :name, :content
+  # accepts_nested_attributes_for :tags, reject_if: proc {
+  #               |attributes| attributes['name'].blank? }
+  # accepts_nested_attributes_for :tags, reject_if: :all_blank
+
+  def tags_attributes=(tags_hashes)
+    tags_hashes.each do |i, tag_attributes|
+      if tag_attributes[:name].present?
+        tag = Tag.find_or_create_by(name: tag_attributes[:name])
+        self.post_tags.build(:tag => tag)
+      end
+    end
+  end
 end
