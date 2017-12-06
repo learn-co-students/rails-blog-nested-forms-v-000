2e90830b0faf40a87a4c2906ed7a8a48aeafd25b
chrisjamesr <chrisjames.r@gmail.com>
2017-12-05 20:53:11 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,22 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_tags
   has_many :tags, :through => :post_tags
-
   validates_presence_of :name, :content
+  validates_presence_of :tags, allow_blank: false
+
+
+
+  def tags_attributes=(tag_hash)
+    if tag_hash[:name].present?
+      tag = Tag.find_or_create_by(name: tag_hash[:name])
+      if !self.tags.include?(tag) 
+        self.post_tags.build(:tag => tag)
+      end
+    end
+  end
+
+
+
+
+
 end
