f3fe4247b02a26caccc47e7007de63536df337e2
Olena Ageyeva <eageeva@hotmail.com>
2018-01-26 03:39:17 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,14 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_tags
   has_many :tags, :through => :post_tags
+  accepts_nested_attributes_for :tags
 
   validates_presence_of :name, :content
+
+  def tags_attributes(tag_hash)
+    binding.pry
+    if tag_hash[:name].present?
+      Tag.find_or_create_by(tag_hash[:name])
+    end
+  end
 end
