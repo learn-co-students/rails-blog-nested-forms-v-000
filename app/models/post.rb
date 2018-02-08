d8df56b30148290acd0cd35b1fba562cd11e59f0
Richard Steinbach <steinbach.dick@gmail.com>
2018-02-08 11:34:25 -0800

    rails-blog-nested-forms-v-000

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,25 @@
+require 'pry'
 class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   has_many :post_tags
   has_many :tags, :through => :post_tags
+  accepts_nested_attributes_for :tags, reject_if: lambda {|attributes| attributes['name'].blank?}
 
   validates_presence_of :name, :content
+
+  #def tags_attributes=(tag_attributes)
+    #binding.pry
+    #if tag_attributes.present?
+      #tag_attributes.values.each do |t|
+        #if t[:name].present?
+          #tag = Tag.find_or_create_by(t)
+          #self.tags << tag
+        #end
+      #end
+    #end
+  #end
+
+
+
 end
