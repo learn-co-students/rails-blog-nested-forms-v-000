0e836ea57ccc9ac3f4db22f0a180f8fbe8e740e9
miriam <miriam5000@gmail.com>
2018-02-17 22:57:39 -0500

    fix save-without-new-category

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,5 @@ class User < ActiveRecord::Base
   validates_presence_of :name
 
   accepts_nested_attributes_for :posts, reject_if: proc { |attributes| attributes['title'].blank?}
-	end 
-
-
+	
 end
