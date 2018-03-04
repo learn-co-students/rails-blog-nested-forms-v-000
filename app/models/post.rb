a6a2fb6a8547bb1b29a081a1bb0012418543c7a1
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-04 18:40:14 +0100

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,7 @@ class Post < ActiveRecord::Base
   accepts_nested_attributes_for :tags, reject_if: proc { |attributes| attributes['name'].blank? }
 
   def tags_attributes=(attr)
-    if !attr[0].empty?
+    if !attr.first.empty?
       attr.values.each do |a|
         a = Tag.find_or_create_by(a)
         self.tags << a
