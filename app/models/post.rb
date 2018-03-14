0cf03c955711970c95509d6e7c58b42ae242b691
catalanowebdevelopment <catalano.work@gmail.com>
2018-03-14 14:44:22 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,9 @@
 class Post < ActiveRecord::Base
-  belongs_to :user
-  has_many :comments
-  has_many :post_tags
-  has_many :tags, :through => :post_tags
+    belongs_to :user
+    has_many :comments
+    has_many :post_tags
+    has_many :tags, :through => :post_tags
+    accepts_nested_attributes_for :tags
 
-  validates_presence_of :name, :content
+    validates_presence_of :name, :content
 end
