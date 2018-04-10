c9985b7e570f2583aa1d73afca6eb6e9fb7b5138
jendobes <jdobak1@gmail.com>
2018-04-10 16:02:14 +0200

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :posts
+  accepts_nested_attributes_for :posts, reject_if: proc { |attributes| attributes['title'].blank? }
   has_many :comments
 
   validates_uniqueness_of :name
