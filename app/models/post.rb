8de1bacfb298bb08c2bf97ea446ce3308064d90a
Rygel-XVI <micha232@umn.edu>
2018-04-10 03:15:34 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,24 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_tags
   has_many :tags, :through => :post_tags
+  accepts_nested_attributes_for :tags, reject_if: proc {|attribute| attribute['name'].blank?}
 
   validates_presence_of :name, :content
+
+
+# works, now never do this again
+
+#   def tags_attributes=(tag_attributes)
+#     if defined?tag_attributes.values
+#       tag_attributes.values.each do |attribute|
+#         if attribute[:name].present?
+#           tag = Tag.find_or_create_by(attribute)
+#           if !self.tags.include?(tag)
+#             self.tags << tag
+#           end
+#         end
+#       end
+#     end
+#   end
+
 end
